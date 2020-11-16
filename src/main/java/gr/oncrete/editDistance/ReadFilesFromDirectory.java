9978c8dcf97246f9ada5ed1007176b08861e53ea
nickapos <nickapos@oncrete.gr>
2020-11-16 23:08:02 +0000

    Adding editDistance example and read directory class

---
+++ src/main/java/gr/oncrete/editDistance/ReadFilesFromDirectory.java
@@ -0,0 +1,33 @@
+package gr.oncrete.editDistance;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReadFilesFromDirectory {
+    List<String> filenames;
+    String scanPath;
+
+    public ReadFilesFromDirectory(String scanPath) {
+        filenames = new ArrayList<>();
+        this.scanPath = scanPath;
+    }
+
+    public void readFiles() {
+
+        try (Stream<Path> paths =Files.walk(Paths.get(this.scanPath))){
+            filenames=paths.filter(Files::isRegularFile).map(Path::toString).collect(Collectors.toList());
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void printFilenames(){
+        filenames.forEach(name -> System.out.println(name) );
+    }
+}
